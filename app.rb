a924b1af206e1d7f4abe725908fd6dc40bc6159b
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-04-29 19:13:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+    get "/newteam" do
+        erb :newteam 
+    end
+
+    post "/newteam" do
+
+        @hash = params 
+=begin
+         @team_name = params[:name]
+         @coach = params[:coach]
+         @pg = params[:pg]
+         @sg = params[:sg]
+         @sf = params[:sf]
+         @pf = params[:pf]
+         @c = params[:c]
+=end 
+         erb :team 
+    end
 
 end
